93c7a3ebe49d3f215086504a01e9dcc601e4d8c8
SamirOuarrak <samir.ouarrak@gmail.com>
2023-03-16 14:43:24 -0700

    AutoPush

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include <unistd.h>
+#include <stdio.h>
 
 int main(void)
 {
-	write(1, "\"Programming is like building a multilingual puzzle",51);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
