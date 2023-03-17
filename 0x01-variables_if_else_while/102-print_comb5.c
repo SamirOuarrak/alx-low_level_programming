adc38ce89898fe6294b78110369150715235270d
SamirOuarrak <samir.ouarrak@gmail.com>
2023-03-17 16:02:00 -0700

    AutoPush

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '8'; b++)
+		{
+			c = a;
+			d = b + 1;
+			for (; c <= '9'; c++)
+			{
+
+				for (;d <= '9'; d++)
+				{
+					if (a != j && j != k)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d):
+						if (a != '9' || b != '8' || c != '9')
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
