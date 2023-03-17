0297f92e22b3c44208c318728928fce993c18d83
SamirOuarrak <samir.ouarrak@gmail.com>
2023-03-17 15:37:54 -0700

    AutoPush

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = '0'; i <= '7'; i++)
+	{
+		for (j = '1'; j <= '8'; j++)
+		{
+			for (k = '2'; k <= '9'; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != '7' || j != '8' || k != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			k++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
