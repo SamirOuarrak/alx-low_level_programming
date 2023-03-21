4dcc1fe42357899fe371358b2a7ecab9f817d49d
SamirOuarrak <samir.ouarrak@gmail.com>
2023-03-21 16:04:52 -0700

    AutoPush

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i * j < 10)
+			{
+				_putchar('0' + j * i);
+			}
+			else
+			{
+				_putchar('0' + j * i / 10);
+				_putchar('0' + j * i % 10);
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (i * j < 10)
+				{
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
