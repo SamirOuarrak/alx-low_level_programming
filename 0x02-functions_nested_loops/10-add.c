471e42dc6fd5875766857aa6560e213caaad0dc5
SamirOuarrak <samir.ouarrak@gmail.com>
2023-03-21 16:15:56 -0700

    AutoPush

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add -  adds two integers and returns the result.
+ * @a: numb 1 to add
+ * @b: numb 2 to add
+ * Return: results a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
