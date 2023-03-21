fb4711e740eb5e0cd0562f8badc2a09221b9822f
SamirOuarrak <samir.ouarrak@gmail.com>
2023-03-21 13:13:59 -0700

    AutoPush

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * _islower - check if 'c' is lower
- * Returns 1 if c is lowercase
- * Returns 0 otherwise
+ * @c: char to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-	if (c >= 'a'&& c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
-	return(0);
+	return (0);
 }
