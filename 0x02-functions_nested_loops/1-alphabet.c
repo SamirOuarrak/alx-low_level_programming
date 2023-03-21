b7296bdc3292a52af200e77744430dca42ff2933
SamirOuarrak <samir.ouarrak@gmail.com>
2023-03-21 12:59:54 -0700

    AutoPush

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _ - print alphabet
+ * print_alphabet - print alphabet
  *
  * Return: On success 0.
  */
@@ -8,7 +8,7 @@ void print_alphabet(void)
 {
 	int i;
 
-	for (i='a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
