3aee7157e033690c2faf098243bb9df41fcdbbad
SamirOuarrak <samir.ouarrak@gmail.com>
2023-03-21 18:39:36 -0700

    AutoPush

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * bonacci - bonacci calculate.
+ * @a : first number (1)
+ * @b : Second number (2)
+ * @count : count recurssion
+ */
+
+void bonacci(unsigned long a, unsigned long b, int count)
+{
+	if (a == 1)
+	{
+		printf("1, ");
+	}
+	if (count < 98)
+	{
+		if (a != 1)
+		{
+			printf(", ");
+		}
+		printf("%lu", b);
+		bonacci(b, a + b, count + 1);
+	}
+}
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	bonacci(1, 2, 1);
+	printf("\n");
+	return (0);
+}
