9f48b02eec7fd1af9335833890a0dd2f833ffede
SamirOuarrak <samir.ouarrak@gmail.com>
2023-03-21 17:18:56 -0700

    AutoPush

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * _putnb - putnb
+ * @n: number to put
+ */
+void _putnb(int n)
+{
+	if (n <= 9)
+	{
+		_putchar ('0' + n);
+	}
+	else
+	{
+		_putnb(n / 10);
+		_putchar('0' + n % 10);
+	}
+}
+/**
+ * print_times_table - prints the 9 times table, starting with 0.
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (i * j <= 9 && j != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (i * j <= 99 && j != 0)
+			{
+				_putchar(' ');
+			}
+			_putnb(i * j);
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
