b74a22e36bfe551619efd7efd0ea0019e0e1c9dd
SamirOuarrak <samir.ouarrak@gmail.com>
2023-03-21 18:34:45 -0700

    AutoPush

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,12 +8,14 @@ int main(void)
 {
 	int a = 1;
 	int b = 2;
+	int c = 0;
 	int sum = 2;
 
 	while (b <= 4000000)
 	{
+		c = a + b;
 		a = b;
-		b =+ a;
+		b = c;
 		if (b % 2 == 0)
 		{
 			sum += b;
