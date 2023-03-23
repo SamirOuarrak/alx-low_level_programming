f714b6e5e2a29a051b92dd9558ef58f262fd71cf
SamirOuarrak <samir.ouarrak@gmail.com>
2023-03-23 14:44:14 -0700

    AutoPush

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Fizz-Buzz
+ * Return: always 1
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s", "FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s", "Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s", "Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
