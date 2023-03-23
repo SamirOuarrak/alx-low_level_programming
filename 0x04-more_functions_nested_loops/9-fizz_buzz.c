64dc16c4d742a43ff80781ade516019de48b6b08
SamirOuarrak <samir.ouarrak@gmail.com>
2023-03-23 14:46:47 -0700

    AutoPush

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,23 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("%s", "FizzBuzz ");
+			printf("%s", "FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("%s", "Buzz ");
+			printf("%s", "Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("%s", "Fizz ");
+			printf("%s", "Fizz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
 		}
 		i++;
 	}
