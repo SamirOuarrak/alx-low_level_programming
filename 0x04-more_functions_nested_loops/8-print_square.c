6c1def0e4602b708fdecfc0d8fc060e681e7aec3
SamirOuarrak <samir.ouarrak@gmail.com>
2023-03-23 14:34:56 -0700

    AutoPush

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - print diagonal
+ * @n: nb to print as _
+ */
+void print_square(int n)
+{
+	int i;
+	int j;
+
+	j = 0;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	while (j < n)
+	{
+		i = 0;
+		while (i < n)
+		{
+			_putchar('#');
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
