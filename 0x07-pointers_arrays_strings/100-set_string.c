143141dd71d4be3efcffa843d547b94a23ea25ed
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-03 18:19:41 -0700

    AutoPush

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string- set_string
+ * @s: source
+ * @to: to
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagsums - print sum of diag
+ * @a: Chessboard
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int j;
+	int sumX;
+	int sumY;
+
+	i = 0;
+	j = 0;
+	sumX = 0;
+	sumY = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			j++;
+		}
+		i++;
+	}
+}
