a747d911806624dc22c04fef193143311d38fd05
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-06 17:07:20 -0700

    AutoPush

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,17 +8,11 @@
  */
 int _sqrt_recursion_helper(int n, int low, int high)
 {
-	int mid;
-
-	if (low > high)
+	if (low * low == n)
+		return (low);
+	else if (low * low > n || low > high)
 		return (-1);
-	mid = (low + high) / 2;
-	if (mid * mid == n)
-		return (mid);
-	else if (mid * mid > n)
-		return (_sqrt_recursion_helper(n, low, mid - 1));
-	else
-		return (_sqrt_recursion_helper(n, mid + 1, high));
+	return (_sqrt_recursion_helper(n, low + 1, high));
 }
 
 /**
@@ -29,10 +23,5 @@ int _sqrt_recursion_helper(int n, int low, int high)
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	else if (n == 0 || n == 1)
-		return (n);
-	else
-		return (_sqrt_recursion_helper(n, 1, n / 2));
+	return (_sqrt_recursion_helper(n, 1, n / 2));
 }
