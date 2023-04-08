d5da7e06c9ef07b84a3412b71885ce84b4d38403
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-08 13:37:07 -0700

    AutoPush

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy- copy str
+ * @dest: destination
+ * @src: src
+ * Return: destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src && (*(src + i) != '\0'))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
