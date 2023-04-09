c6af0357509fef52e65896487ef373d67ecbfe80
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-08 17:13:28 -0700

    AutoPush

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,26 @@
-#include <stdio.h>
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - puts
+ * @str: str
+ */
+void _puts(char *str)
+{
+	while (str && *str)
+		_putchar(*str++);
+}
+
 /**
  * main - check the code
  * @argc: argc
@@ -12,7 +34,8 @@ int main(int argc, char *argv[])
 	i = 0;
 	while (i < argc)
 	{
-		printf("%s\n", argv[i]);
+		_puts(argv[i]);
+		_putchar('\n');
 		i++;
 	}
 	return (1);
