830e5197caa9923321f99bb009ab5e21e264e1d2
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-08 17:44:28 -0700

    AutoPush

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,12 +9,25 @@
 int main(int argc, char __attribute__((unused)) *argv[])
 {
 	int sum;
+	int i;
 
 	sum = 0;
 	if (argc > 1)
 	{
 		while (argc-- > 0)
+		{
+			i = 0;
+			while argv[argc][i]
+			{
+				if argv[argc][i] < '0' ||  argv[argc][i] > '9'
+				{
+					printf("Error\n");
+					return(0);
+				}
+				i++:
+			}
 			sum += atoi(argv[argc]);
+		}
 		printf("%d\n", sum);
 	}
 	else
