f12f48f1d85af588849581eac7c0db2b5bcc721f
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-08 17:25:11 -0700

    AutoPush

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: argv
  * Return: Always 0.
  */
-int main(int argc, char *__attribute__((unused)) argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc);
 	return (0);
