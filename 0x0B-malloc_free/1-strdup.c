a7c8900ead13d095665b6f948716a3138c17e41e
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-11 13:21:02 -0700

    AutoPush

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - duplicate string
+ * @str: string to duplicate
+ * Return: new string
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	size_t i;
+
+	if (str == NULL)
+		return (NULL);
+	dup = malloc(sizeof(char) * strlen(str) + 1);
+	if (dup != NULL)
+	{
+		for (i = 0; i < strlen(str); i++)
+			dup[i] = str[i];
+	}
+	dup[i] = '\0';
+	return (dup);
+}
