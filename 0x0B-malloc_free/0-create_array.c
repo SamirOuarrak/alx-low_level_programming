d97c7fea52c37a8838f6bfc850224fdf4744e734
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-11 13:03:22 -0700

    AutoPush

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array- creates an array of chars
+ * @size: size
+ * @c: Char
+ * Return: string
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	str = malloc(sizeof(unsigned int) * size + 1);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	str[i] = '\0';
+	return (str);
+}
