6fbde38e364ecc88c4ba0e945c2f9bd65e2a638f
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-11 14:03:36 -0700

    AutoPush

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,12 @@ int **alloc_grid(int width, int height)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
+		{
+			while (i > 0)
+				free(grid[i - 1]);
+			free(grid);
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
 	}
