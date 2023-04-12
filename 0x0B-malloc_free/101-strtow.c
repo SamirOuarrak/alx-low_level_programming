a5d87b6bad194abd2d9f84c4607da4548ea72877
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-11 17:55:51 -0700

    AutoPush

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,92 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * countWord - count word
+ * @str: string
+ * Return: count word
+ */
+
+int countWord(char *str)
+{
+	int count;
+
+	count = 0;
+	while (*str && *str++ == ' ')
+		;
+	while (*str)
+	{
+		if (*str == ' ' && *(str + 1))
+			count++;
+		while (*str && *str++ == ' ')
+			;
+	}
+	return (count);
+}
+
+/**
+ * getLen - getLen word
+ * @str: string
+ * Return: len next word
+ */
+
+int getLen(char *str)
+{
+	int count;
+
+	count = 0;
+	while (*str && *str == ' ')
+		str++;
+	while (*str && *str != ' ')
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
+/**
+ * strtow - splits a string into words.
+ * @str: string
+ * Return: string
+ */
+char **strtow(char *str)
+{
+	char **arr;
+	int i;
+	int j;
+
+	if (!str || !*str)
+		return (NULL);
+	arr = malloc(sizeof(*arr) * (countWord(str) + 1));
+	if (arr == NULL)
+		return (NULL);
+	i = 0;
+	while (*str)
+	{
+		j = 0;
+		while (*str && *str == ' ')
+			str++;
+		if (!*str)
+			break;
+		arr[i] = malloc(sizeof(**arr) * getLen(str) + 1);
+		if (arr[i] == NULL)
+		{
+			while (i > 0)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
+		while (*str && *str != ' ')
+		{
+			arr[i][j] = *str;
+			str++;
+			j++;
+		}
+		arr[i][j] = '\0';
+		while (*str && *str == ' ')
+			str++;
+		i++;
+		}
+	arr[i] = NULL;
+	return (arr);
+}
