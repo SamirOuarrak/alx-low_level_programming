156d4d8043422207b9c56c1dcbaa60eed40d0cb1
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-13 18:35:24 -0700

    AutoPush

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - malloc checkd
+ * @b: b
+ * Return: string
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(b));
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
