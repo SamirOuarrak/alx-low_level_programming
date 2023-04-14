d19a016a2e9ea8c461a404dedffecc7a819163df
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-13 19:43:03 -0700

    AutoPush

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * main- main
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
