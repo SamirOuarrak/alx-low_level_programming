6b5bc6ac63950fa4f8d58affc18e434fb45fe564
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-13 19:12:49 -0700

    AutoPush

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,9 +11,15 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	size_t count:
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = (void *)malloc(nmemb * size);
+	count = nmemn * size;
+	while (count-- > 0)
+	{
+		ptr[count] = 0;
+	}
 	return (ptr);
 }
