adefa60d34eeb27d7f7163d2e1402ec3f55ad98d
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-16 12:38:36 +0000

    AutoPush

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,13 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
+/**
+ * ParseNumbers- ParseNumbers
+ * @ac: argc
+ * @av: argv
+ * Return: true/false
+ */
+
+int ParseNumbers(int ac, char **av)
+{
+	if (ac != 3)
+		return (0);
+	return (1);
+}
+
 /**
  * main- main
  * @argc: argc
  * @argv: argv
  * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
+	
 	return (0);
 }
