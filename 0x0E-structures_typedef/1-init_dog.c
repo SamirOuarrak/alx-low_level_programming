0c8178d27dc251ccba3bd1300fcb5cbb411abf5a
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-17 10:43:53 +0000

    AutoPush

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - init_dog
+ * @d: struct dog
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner name
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->age = age;
+	d->name = name;
+	d->owner = owner;
+}
