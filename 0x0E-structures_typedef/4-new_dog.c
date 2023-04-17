0bf59ec15b9a84690c2fba91bb70734cf133790a
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-17 11:13:50 +0000

    AutoPush

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,27 @@
 #include "dog.h"
 
+ /**
+  * _strdup - duplicate string
+  * @str: string to duplicate
+  * Return: new string
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	size_t i;
+
+	if (str == NULL)
+		return (NULL);
+	dup = malloc(sizeof(char) * strlen(str) + 1);
+	if (dup != NULL)
+	{
+		for (i = 0; i < strlen(str); i++)
+			dup[i] = str[i];
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
 /**
  * new_dog - new_dog
  * @name: name
@@ -13,6 +35,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *myDog;
 
 	myDog = malloc(sizeof(dog_t));
+	if (!myDog)
+		return (NULL);
 	myDog->age = age;
 	myDog->name = name;
 	myDog->owner = owner;
