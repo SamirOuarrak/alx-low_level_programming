c65a763e62ef2dfad0cd5159f7d391c3bda1b89d
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-17 10:38:19 +0000

    AutoPush

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ */
+typedef struct dog
+{
+	char *name;
+	double age;
+	char *owner;
+} dog_t;
+#endif
