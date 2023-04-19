56827d52c48ac51a19dfab6f635266ee824be505
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-19 20:40:15 +0000

    AutoPush

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,11 +12,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (!get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])))
+	if (!get_op_func(argv[2]))
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
 	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
