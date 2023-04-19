e9348c2337ba078926354e46a0944469f7c5556e
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-19 21:00:46 +0000

    AutoPush

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,8 +20,9 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (i < 5)
 	{
-		if (s[0] == ops[i].op[0])
-			return (ops[i++].f);
+		if (!strcmp(s, ops[i].op))
+			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
