fdd84f911058eb6d4f01f57a8c02992f2b7fc231
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-20 15:51:16 +0000

    AutoPush

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,19 +11,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	va_list ap;
 
-	if (!n || !separator)
+	if (n && separator)
 	{
-		printf("\n");
-		return;
+		va_start(ap, n);
+		while (i < n)
+		{
+			printf("%d", va_arg(ap, int));
+			if (++i < n)
+				printf("%s", separator);
+		}
 	}
-	va_start(ap, n);
-	while (i < n)
-	{
-		printf("%d", va_arg(ap, int));
-		if (++i < n)
-			printf("%s", separator);
-		else
-			printf("\n");
-	}
+	printf("\n");
 	va_end(ap);
 }
