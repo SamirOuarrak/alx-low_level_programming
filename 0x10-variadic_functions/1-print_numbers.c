7104dc45da4073693af6614f658157b311fcff4a
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-20 15:42:49 +0000

    AutoPush

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - print_numbers the code
+ * @separator : separator
+ * @n: number of va_arg
+ * @... : varg list
+ *  Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list ap;
+
+	va_start(ap, n);
+	while (i++ < n)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i < n)
+			printf("%s", separator);
+		else
+			printf("\n");
+	}
+	va_end(ap);
+}
