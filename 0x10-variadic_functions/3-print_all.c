e10b0ede67566b24538d2ac7ec1a37cf16132ffd
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-20 22:19:24 +0000

    AutoPush

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,66 @@
+#include "variadic_functions.h"
+/**
+ * Dispatcher - Dispatcher
+ * @c: format
+ * @ap : varg list
+ *  Return: Always 0.
+ */
+
+int Dispatcher(char c, va_list ap)
+{
+	char *str;
+
+	switch (c)
+	{
+		case 'i':
+			printf("%d", va_arg(ap, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, double));
+			break;
+		case 'c':
+			printf("%c", va_arg(ap, int));
+			break;
+		case 's':
+			str = va_arg(ap, char *);
+			if (str)
+			{
+				printf("%str", str);
+				return (1);
+			}
+			printf("(nil)");
+	}
+	return (1);
+}
+
+/**
+ * print_all - print_all
+ * @format: format va_arg
+ * @... : varg list
+ *  Return: Always 0.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	unsigned int i = 0;
+	unsigned int j;
+	unsigned int isToSep = 0;
+	const char arg[] = "cifs";
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (arg[j])
+		{
+			if (format[i] == arg[j++] && isToSep)
+			{
+				printf(", ");
+				break;
+			}
+		}
+		isToSep = Dispatcher(format[i++], ap);
+	}
+	printf("\n");
+	va_end(ap);
+}
