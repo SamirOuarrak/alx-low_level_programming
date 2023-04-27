d69c50ce8dccb90321bb8b0c169cbf601236cdc1
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-27 20:07:43 +0000

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * before_main - before_main
+ *
+ */
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
