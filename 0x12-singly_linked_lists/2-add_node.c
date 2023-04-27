d4524f6365df93d79052028d9889de5ea03559b8
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-27 18:43:38 +0000

    Task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_node - number of elements in a linked
+ * @head : head of list_t
+ * @str : str
+ *  Return: the head
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = NULL;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = str ? strlen(str) : 0;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
