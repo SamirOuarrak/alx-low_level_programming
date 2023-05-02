d3f2623d7330dee3c844fd814ca599f96bab8e3b
SamirOuarrak <samir.ouarrak@gmail.com>
2023-05-03 00:22:40 +0100

    AutoPush

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	while (ptr->next && i >= 0)
+	while (ptr && i >= 0)
 	{
 		if (!i--)
 		{
