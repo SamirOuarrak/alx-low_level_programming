c64a920415cf8666035140ca756144ada9dfd28a
SamirOuarrak <samir.ouarrak@gmail.com>
2023-05-03 01:10:01 +0100

    AutoPush

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,31 @@
+#include "list.h"
+
+/**
+ * free_listint_safe - Frees a listint_t linked list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current, *next;
+	size_t count = 0;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	current = *h;
+	*h = NULL;
+
+	while (current != NULL)
+	{
+		count++;
+		next = current->next;
+		free(current);
+		if (next >= current)
+			return (0);
+		current = next;
+	}
+
+	return (count);
+}
