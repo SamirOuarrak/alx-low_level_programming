4f7a07dff92459833f7ebc229c28c1165a168500
SamirOuarrak <samir.ouarrak@gmail.com>
2023-05-27 09:17:27 +0100

    AutoPush

---
+++ 0x15-file_io/1-create_fil.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_file - create_file.
+ * @filename: filename
+ * @text_content: text_content
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0, file;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	while (!text_content[i])
+		i++;
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+	write(file, text_content, i);
+	return (1);
+}
