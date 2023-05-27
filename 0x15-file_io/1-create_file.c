06dd1604851aec703dff3b883802237238bcdf97
SamirOuarrak <samir.ouarrak@gmail.com>
2023-05-27 09:22:38 +0100

    AutoPush

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,14 +9,13 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i = 0;
-	int file;
+	int i = 0, file;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	if (!text_content)
+	if (text_content == NULL)
 		text_content = "";
-	while (!text_content[i])
+	while (text_content[i] != '\0')
 		i++;
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
