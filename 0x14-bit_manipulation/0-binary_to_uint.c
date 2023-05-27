184308eda2c3c52d45a1f69a87c16348c2d853cf
SamirOuarrak <samir.ouarrak@gmail.com>
2023-05-27 10:02:57 +0100

    AutoPush

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - binary_to_uint
+ * @b: b
+ * Return: ret
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int nb;
+	int j, i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i])
+	{
+		if (b[i] && b[i] != '1')
+			return (0);
+		i++;
+	}
+	nb = 0;
+	for (j = 0; b[j]; j++)
+	{
+		nb = nb << 1;
+
+		if (b[j] == '1')
+			nb = nb + 1;
+	}
+	return (nb);
+}
